3a15ad7c5b0ee55c335bd4fc59eb8f998c3dbf0a
Matthew Gotth-Olsen <Matthew.Gotth-Olsen@louisvilleky.gov>
2018-01-02 20:43:58 -0500

    adding function for medium blog
    Need to configure lambda function per https://medium.com/statuscode/building-a-basic-web-service-to-display-your-medium-blog-posts-on-your-website-using-aws-api-48597b1771c5

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -6,61 +6,88 @@
  * @return N/A
  */
 
- var parseProjectsJSON = function(post_rows) {
-   // max 10 posts
-   post_rows = post_rows || 10;
-
-   // get the template
-   var template_markup = $('#project-template').html();
-
-   // get the data
-   $.getJSON('http://projects.lsvll.io/recent-projects.json', function(posts) {
-     for (var i = 0; i < posts.length; ++i) {
-       var post = posts[i];
-       // for each post, until we hit max
-       // fill out the variables
-       if (i < post_rows ) {
-         var thisInstance = $(template_markup);
-           thisInstance.find('.tile-title').text(post.title);
-           thisInstance.find('.tile-subtitle').text(post.subtitle);
-           thisInstance.find('.tile-link').attr('href', post.url);
-           thisInstance.find('.tile-img').css('background-image', 'url(' + post.img +')');
-           thisInstance.find('.tile-date').text(post.date);
-           thisInstance.find('.tile-excerpt').text(post.excerpt);
-         $('#projects-container').append(thisInstance);
-         // and append to the container
-       }
-     }
-   });
- }
-
  var parseMediumJSON = function(post_rows) {
    // max 10 posts
    post_rows = post_rows || 10;
 
    // get the template
-   var template_markup = $('#project-template').html();
+   var template_markup = $('#post-template').html();
+ // get the data
+ $.getJSON('https://medium.com/louisville-metro-opi2/latest?format=json', function(data) {
+   var author_list = data.payload.references.User;
+   var all_posts = data.payload.posts;
+   var posts_to_output = $.map(all_posts, function(post, index){
+     var author = author_list[post.creatorId];
+     var cleaned_post = {
 
-   // get the data
-   $.getJSON('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Flouisville-metro-opi2', function(posts) {
-     for (var i = 0; i < posts.length; ++i) {
-       var post = posts[i];
-       // for each post, until we hit max
-       // fill out the variables
-       if (i < post_rows ) {
-         var thisInstance = $(template_markup);
-           thisInstance.find('.tile-title').text(post.title);
-           thisInstance.find('.tile-subtitle').text(post.subtitle);
-           thisInstance.find('.tile-link').attr('href', post.url);
-           thisInstance.find('.tile-img').css('background-image', 'url(' + post.img +')');
-           thisInstance.find('.tile-date').text(post.date);
-           thisInstance.find('.tile-excerpt').text(post.excerpt);
-         $('#post-container').append(thisInstance);
-         // and append to the container
-       }
+       post_title:       post.title,
+
+                         // get full URL, prepend Medium collection
+       post_url:         "https://medium.com/louisville-metro-opi2/" + post.uniqueSlug,
+
+                         // get full Image URL, append Medium CDN
+       post_image_url:   "https://cdn-images-1.medium.com/max/1000/" + post.virtuals.previewImage.imageId,
+
+                         // get author name by ID, from user_list
+       post_author_name: author.name,
+
+                         // get author pic by ID
+       post_author_pic:  "https://cdn-images-1.medium.com/fit/c/60/60/" + author.imageId,
+
+                         // convert date to human-readable string
+       post_date:        new Date(post.firstPublishedAt).format('F j, Y')
+
+     };
+
+     // for each post, until we hit max
+     // fill out the variables
+     if (index < post_rows ) {
+       var thisInstance = $(template_markup);
+         thisInstance.find('.tile-title').text(cleaned_post.post_title);
+         thisInstance.find('.tile-link').attr('href', cleaned_post.post_url);
+         thisInstance.find('.tile-img').css('background-image', 'url(' + cleaned_post.post_image_url +')');
+         thisInstance.find('.tile-author-pic').attr('src', cleaned_post.post_author_pic).attr('alt', cleaned_post.post_author_name);
+         thisInstance.find('.tile-author').text(cleaned_post.post_author_name);
+         thisInstance.find('.tile-date').text(cleaned_post.post_date);
+       $('#post-container').append(thisInstance);
+       // and append to the container
      }
+
+     return cleaned_post;
+     console.log(all_posts);
    });
- }
+});
+}
+
+
+var parseProjectsJSON = function(post_rows) {
+  // max 10 posts
+  post_rows = post_rows || 10;
+
+  // get the template
+  var template_markup = $('#project-template').html();
+
+  // get the data
+  $.getJSON('http://projects.lsvll.io/recent-projects.json', function(posts) {
+    for (var i = 0; i < posts.length; ++i) {
+      var post = posts[i];
+      // for each post, until we hit max
+      // fill out the variables
+      if (i < post_rows ) {
+        var thisInstance = $(template_markup);
+          thisInstance.find('.tile-title').text(post.title);
+          thisInstance.find('.tile-subtitle').text(post.subtitle);
+          thisInstance.find('.tile-link').attr('href', post.url);
+          thisInstance.find('.tile-img').css('background-image', 'url(' + post.img +')');
+          thisInstance.find('.tile-date').text(post.date);
+          thisInstance.find('.tile-excerpt').text(post.excerpt);
+        $('#projects-container').append(thisInstance);
+        // and append to the container
+      }
+    }
+  });
+}
+
 
 function togglePressed(element) {
 
@@ -119,8 +146,6 @@ var initLinkAttributes = function() {
 
   });
 }
-
-
 
 $(document).ready(function(){
   initMobileMenus();
